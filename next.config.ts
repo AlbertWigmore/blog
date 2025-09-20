9a8c1d65d057384bf957380f7e91624a240429f6
albert.wigmore <albert@open-cosmos.com>
2025-09-20 14:44:23 +0100

    fix: make app configurable for standalone to fix docker build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
 
+module.exports = {
+  output: "standalone",
+};
 
 const nextConfig: NextConfig = {
   pageExtensions: ["tsx", "ts", "js", "jsx", "md", "mdx"],

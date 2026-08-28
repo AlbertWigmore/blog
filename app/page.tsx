b99f286b75a6f50596b06ee0f8daf1a6c80adbf8
albert.wigmore <albert@open-cosmos.com>
2026-08-28 21:05:13 +0100

    fix: update the formatting on the front page to improve the alignment

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import { getAllPosts, getAllTILs } from "@/lib/content";
 import { FC } from "react";
 import { PostCard } from "@/app/components/post-card";
 import { TILCard } from "@/app/components/til-card";
-import { Image} from "@heroui/image";
+import { Image } from "@heroui/image";
 
 const HomePage: FC = () => {
   const posts = getAllPosts();
   const tils = getAllTILs();
 
   return (
-
     <div className="flex flex-col">
       <div className="flex flex-row items-center">
         <Image
@@ -22,49 +21,43 @@ const HomePage: FC = () => {
         <p className="text-center p-10">I&apos;m Albert, this is my personal blog where I write on various topics from hobbies to work.</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
         <div className="flex flex-col gap-4">
           <h2 className="text-2xl font-bold">Posts</h2>
-          <div className="overflow-y-auto overflow-hidden h-full" style={{ height: "500px", scrollbarWidth: "none"}}>
-            <ul className="space-y-4">
-              {posts.map((post) => (
-                <li key={post.urlSlug}>
-                  <PostCard
-                    title={post.title}
-                    date={post.date}
-                    excerpt={post.excerpt}
-                    tags={post.tags}
-                    urlSlug={post.urlSlug}
-                    imageUrl={post.imageUrl}
-                  />
-                </li>
-              ))}
-            </ul>
+          <div className="flex flex-col gap-4">
+            {posts.map((post) => (
+              <PostCard
+                key={post.urlSlug}
+                title={post.title}
+                date={post.date}
+                excerpt={post.excerpt}
+                tags={post.tags}
+                urlSlug={post.urlSlug}
+                imageUrl={post.imageUrl}
+              />
+            ))}
           </div>
         </div>
-        
+
         <div className="flex flex-col gap-4">
           <h2 className="text-2xl font-bold">TILs</h2>
-          <div className="overflow-y-auto overflow-hidden h-full" style={{ height: "500px", scrollbarWidth: "none"}}>
-            <ul className="space-y-4">
-              {tils.map((til) => (
-                <li key={til.urlSlug}>
-                  <TILCard
-                    title={til.title}
-                    date={til.date}
-                    tags={til.tags}
-                    urlSlug={til.urlSlug}
-                  />
-                </li>
-              ))}
-            </ul>
+          <div className="flex flex-col gap-4">
+            {tils.map((til) => (
+              <TILCard
+                key={til.urlSlug}
+                title={til.title}
+                date={til.date}
+                tags={til.tags}
+                urlSlug={til.urlSlug}
+              />
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default function Page() {
   return <HomePage />;
-}
+};
